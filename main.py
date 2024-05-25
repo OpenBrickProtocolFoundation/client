127736e0c3d8ac2cba988d0aaf8d993b2f984164
Michael Gerhold <michael.gerhold@web.de>
2024-05-25 18:20:05 +0200

    Refactor client communication in main.py
    Implemented asynchronous communication and added new message types in the main.py game client. Essentially, the communication with the game server now uses message broadcasting and the client properly handles received message types. This also includes establishing connections with gameservers and handling potential error scenarios. The refactoring added more structure to the messages being sent and received, which improves the reliability of the client-server communication.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import socket
 import struct
+import sys
 import threading
+import time
 from dataclasses import dataclass
 from enum import Enum
 from typing import NamedTuple
@@ -27,10 +29,15 @@ COLORS = [(0, 0, 0),
 
 RECT_SIZE = 30
 
+done = False
 
-def send_event(target: socket.socket, event: Event) -> None:
-    data = struct.pack("!BQBBQ", 0, 10, event.key.value, event.type.value, event.frame)
-    print(f"sending data {data!r}")
+
+def send_event_buffer(target: socket.socket, events: list[Event], frame: int) -> None:
+    print(f"sending event buffer: {frame=}, {events=}")
+    payload_size = 9 + 10 * len(events)
+    data = struct.pack("!BHQB", 0, payload_size, frame, len(events))
+    for event in events:
+        data += struct.pack("!BBQ", event.key.value, event.type.value, event.frame)
     target.send(data)
 
 
@@ -50,7 +57,10 @@ def render_tetrion(screen: pygame.Surface, position: Vec2, tetrion: Tetrion) -> 
 
 
 class MessageType(Enum):
-    INPUT_EVENT = 0
+    HEARTBEAT = 0
+    GRID_STATE = 1
+    GAME_START = 2
+    EVENT_BROADCAST = 3
 
 
 class MessageHeader(NamedTuple):
@@ -58,27 +68,82 @@ class MessageHeader(NamedTuple):
     payload_size: int
 
 
-event_queue: list[Event] = []
+class ClientEvents(NamedTuple):
+    client_id: int
+    events: list[Event]
+
+
+class BroadcastMessage(NamedTuple):
+    frame: int
+    events_per_client: list[ClientEvents]
+
+
+class GameStartMessage(NamedTuple):
+    client_id: int
+    start_frame: int
+    random_seed: int
+
+
+message_queue: list[BroadcastMessage | GameStartMessage] = []
 
 
 def keep_receiving(server_socket: socket.socket) -> None:
+    global done
     buffer = bytearray()
     current_header: Optional[MessageHeader] = None
-    while True:
-        data = server_socket.recv(4096)
+    print("entering main loop in keep_receiving()")
+    while not done:
+        try:
+            # print("trying to receive message")
+            data = server_socket.recv(4096)
+            print("received message")
+        except BlockingIOError:
+            continue
+        except ConnectionAbortedError:
+            # server has gone :'(
+            print("server has disconnected", file=sys.stderr)
+            break
+        except ConnectionResetError:
+            # most likely we stopped the connection
+            print("ConnectionResetError", file=sys.stderr)
+            break
+
         buffer.extend(data)
-        if current_header is None and len(buffer) >= 9:
-            type_, payload_size = struct.unpack("!BQ", buffer[:9])
-            buffer = buffer[9:]
+        if current_header is None and len(buffer) >= 3:
+            type_, payload_size = struct.unpack("!BH", buffer[:3])
+            buffer = buffer[3:]
             current_header = MessageHeader(type_=MessageType(type_), payload_size=payload_size)
         if current_header is not None and len(buffer) >= current_header.payload_size:
-            assert current_header.type_ == MessageType.INPUT_EVENT
-            key, event_type, frame = struct.unpack("!BBQ", buffer[:current_header.payload_size])
-            buffer = buffer[current_header.payload_size:]
-            event = Event(Key(key), EventType(event_type), frame)
-            event_queue.append(event)
+            print(f"received message of type {current_header.type_.name}")
+
+            match current_header.type_:
+                case MessageType.GAME_START:
+                    client_id, start_frame, random_seed = struct.unpack("!BQQ", buffer[:17])
+                    buffer = buffer[17:]
+                    game_start_message = GameStartMessage(client_id, start_frame, random_seed)
+                    print(game_start_message)
+                    message_queue.append(game_start_message)
+                case MessageType.EVENT_BROADCAST:
+                    message_frame, num_clients = struct.unpack("!QB", buffer[:9])
+                    buffer = buffer[9:]
+                    events_per_client: list[ClientEvents] = []
+                    for _ in range(num_clients):
+                        client_id, event_count = struct.unpack("!BB", buffer[:2])
+                        buffer = buffer[2:]
+                        events: list[Event] = []
+                        for _ in range(event_count):
+                            key, event_type, event_frame = struct.unpack("!BBQ", buffer[:10])
+                            buffer = buffer[10:]
+                            events.append(Event(Key(key), EventType(event_type), event_frame))
+                        events_per_client.append(ClientEvents(client_id, events))
+                    broadcast_message = BroadcastMessage(message_frame, events_per_client)
+                    print(broadcast_message)
+                    message_queue.append(broadcast_message)
+                case _:
+                    raise Exception(f"invalid message type: {current_header.type_.name}")
             current_header = None
-            print(f"Received event: {event}")
+
+    print("main loop ended in keep_receiving()")
 
 
 _LOBBY_URL = "http://127.0.0.1:5000"
@@ -171,33 +236,8 @@ def get_lobby_details(jwt: str, lobby_id: str) -> LobbyResponse:
 
 
 def main() -> None:
-    jwt = login("coder2k", "secret")
-    print(f"{jwt = }")
-
-    lobbies_response = fetch_lobbies()
-    lobby_id: Optional[str] = None
-    for lobby in lobbies_response.lobbies:
-        if lobby.host_info.name == "coder2k":
-            lobby_id = lobby.id
-            print(f"already inside existing lobby: {lobby_id}")
-            break
-
-    if lobby_id is None:
-        lobby_id = create_lobby(jwt, "coder2k's lobby", 4)
-        print(f"Lobby created: {lobby_id}")
-
-    assert lobby_id is not None
-
-    start_game(jwt, lobby_id)
-
-    gameserver_port: Optional[int] = None
-    while True:
-        lobby_details = get_lobby_details(jwt, lobby_id)
-        if lobby_details.gameserver_port is not None:
-            gameserver_port = lobby_details.gameserver_port
-            break
-
-    print(f"{gameserver_port = }")
+    global done
+    gameserver_port = int(input("Enter the port of the gameserver: "))
 
     with (socket.socket(socket.AF_INET, socket.SOCK_STREAM) as gameserver_socket,
           Tetrion() as tetrion,
@@ -205,20 +245,32 @@ def main() -> None:
           ):
         print("trying to connect to gameserver...")
         gameserver_socket.connect(("127.0.0.1", gameserver_port))
+        gameserver_socket.setblocking(False)
         worker_thread = threading.Thread(target=keep_receiving, args=(gameserver_socket,))
         worker_thread.start()
         print("connected to gameserver")
+
+        while len(message_queue) == 0:
+            time.sleep(0.1)
+
+        game_start_message = message_queue.pop(0)
+        assert isinstance(game_start_message, GameStartMessage)
+
+        # todo: use values of game_start_message
+        client_id = game_start_message.client_id
 
         pygame.init()
 
         size = (RECT_SIZE * tetrion.width * 2, (RECT_SIZE + 2) * tetrion.height)
         screen = pygame.display.set_mode(size)
 
-        done = False
-
         clock = pygame.time.Clock()
 
         frame = 0
+
+        event_buffer: list[Event] = []
+
+        other_client_frame: Optional[int] = None
 
         while not done:
             for event in pygame.event.get():
@@ -230,22 +282,28 @@ def main() -> None:
                     elif event.key == pygame.K_a:
                         input_event = Event(key=Key.LEFT, type=EventType.PRESSED, frame=frame)
                         tetrion.enqueue_event(input_event)
-                        send_event(gameserver_socket, input_event)
+                        event_buffer.append(input_event)
                     elif event.key == pygame.K_d:
                         input_event = Event(key=Key.RIGHT, type=EventType.PRESSED, frame=frame)
                         tetrion.enqueue_event(input_event)
-                        send_event(gameserver_socket, input_event)
+                        event_buffer.append(input_event)
                     elif event.key == pygame.K_SPACE:
                         input_event = Event(key=Key.DROP, type=EventType.PRESSED, frame=frame)
                         tetrion.enqueue_event(input_event)
-                        send_event(gameserver_socket, input_event)
+                        event_buffer.append(input_event)
 
             tetrion.simulate_up_until(frame)
 
-            while len(event_queue) > 0:
-                input_event = event_queue.pop(0)
-                other_tetrion.enqueue_event(input_event)
-                other_tetrion.simulate_up_until(input_event.frame)
+            while len(message_queue) > 0:
+                broadcast_message = message_queue.pop(0)
+                assert isinstance(broadcast_message, BroadcastMessage)
+                assert len(broadcast_message.events_per_client) == 2
+                other_client_frame = broadcast_message.frame
+                for input_event in broadcast_message.events_per_client[1 if client_id == 0 else 0].events:
+                    other_tetrion.enqueue_event(input_event)
+
+            if frame > 30 and other_client_frame is not None:
+                other_tetrion.simulate_up_until(min(frame - 30, other_client_frame))
 
             screen.fill((100, 100, 100))
 
@@ -259,7 +317,18 @@ def main() -> None:
             screen.blit(fps_counter, (5, 5 + tetrion.height * RECT_SIZE))
 
             pygame.display.flip()
+
+            if frame % 15 == 0:
+                send_event_buffer(gameserver_socket, event_buffer, frame)
+                event_buffer.clear()
+
             frame += 1
+
+        print("main loop ended")
+
+        worker_thread.join()
+
+        gameserver_socket.close()
 
         pygame.quit()
 
